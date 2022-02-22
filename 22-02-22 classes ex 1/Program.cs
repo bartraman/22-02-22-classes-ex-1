dbb338edb7eb36e40eb12416bd2255cbd685816c
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-02-22 22:38:46 +0100

    Add project files.

---
+++ 22-02-22 classes ex 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _22_02_22_classes_ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
