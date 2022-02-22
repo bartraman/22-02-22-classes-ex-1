90028bce64752b846af74d3d1e2b71b3e6b61160
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-02-22 22:51:06 +0100

    Basics

---
+++ 22-02-22 classes ex 1/Leerling.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _22_02_22_classes_ex_1
+{
+    class Leerling
+    {
+        public string AchterNaamLeerling;
+        public string VoorNaamLeerling;
+        public string Klasleerling;
+
+    }
+}
--- 22-02-22 classes ex 1/Program.cs
+++ 22-02-22 classes ex 1/Program.cs
@@ -6,7 +6,20 @@ namespace _22_02_22_classes_ex_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            do
+            {
+                Leerling gegevens = new Leerling();
+                Console.WriteLine("Wat is de voornaam van de leerling?!");
+                gegevens.VoorNaamLeerling = Console.ReadLine();
+
+                Console.WriteLine("Wat is de achternaam van de leerling?!");
+                gegevens.AchterNaamLeerling = Console.ReadLine();
+
+                Console.WriteLine("Wat is de klas van de leerling?!");
+                gegevens.Klasleerling = Console.ReadLine();
+
+                Console.WriteLine("Wilt u nog een leerling toevoegen? (J/N)");
+            } while (Console.ReadLine().ToUpper().Substring(1, 0) == "J");
         }
     }
 }
